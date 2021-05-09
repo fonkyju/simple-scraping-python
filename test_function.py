cc7ad85db9818b70678b80d4accd4ddecbe40900
Julien K <julien.krier@gmail.com>
2021-05-09 22:18:10 +0200

    Add bad tests

--- test_function.py
+++ test_function.py
@@ -12,20 +12,45 @@ def div():
     div = soup.find(name="div", attrs={"class":"row"})
     return div
 
+@pytest.fixture
+def div2():
+    html = open("./annonces/test.html", "r")
+    soup = BeautifulSoup(html, "lxml", from_encoding="utf-8")
+    div = soup.find(name="div", attrs={"class":"row"})
+    return div
+
 def test_extract_id(div):
     assert extract_id(div) == "987654"
+
+def test_bad_extract_id(div2):
+    assert extract_id(div2) == "Nothing_found"
 
 def test_extract_published(div):
     assert extract_published(div) == 0
 
+def test_bad_extract_published(div2):
+    assert extract_published(div2) == "Nothing_found"
+
 def test_extract_title(div):
     assert extract_title(div) == "Titre de l'annonce 1"
+
+def test_bad_extract_title(div2):
+    assert extract_title(div2) == "Nothing_found"
 
 def test_extract_city(div):
     assert extract_city(div) == "Paris (75)"
 
+def test_bad_extract_city(div2):
+    assert extract_city(div2) == "Nothing_found"
+
 def test_extract_price(div):
     assert extract_price(div) == 233
 
+def test_bad_extract_price(div2):
+    assert extract_price(div2) == "Nothing_found"
+
 def test_extract_url(div):
     assert extract_url(div) == "annonce1.html"
+
+def test_bad_extract_url(div2):
+    assert extract_url(div2) == "Nothing_found"
